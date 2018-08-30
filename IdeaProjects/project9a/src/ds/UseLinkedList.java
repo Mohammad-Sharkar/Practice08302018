81a9fe0a61e6259a8bc8d32b9cf8520ac03fa0cd
nishatsultana <nishatsultana@Nishats-MacBook-Air.local>
2018-08-30 16:45:06 -0400

    hbhggo

---
+++ IdeaProjects/project9a/src/ds/UseLinkedList.java
@@ -0,0 +1,33 @@
+package ds;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class UseLinkedList {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<String>();
+        list.add("NC");
+        list.add("FL");
+        list.add("AZ");
+        list.add("GA");
+        list.add("NJ");
+        System.out.println(list.isEmpty());
+        System.out.println(list.size());
+        System.out.println(list.get(2));
+
+        Iterator it = list.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }  //IF NO ITERATOR, WHAT IS THE OTHER OPTION? DO "FOR LOOP", so 2 choice, forloop or iterator
+        for(String st:list){
+            System.out.println(st);
+        }
+
+
+
+
+
+    }
+}
